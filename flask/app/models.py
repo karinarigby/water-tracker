7a90f85334ca30e3c68e429403956cd45fe81f85
karinarigby <karina.a.rigby@gmail.com>
2021-01-29 23:00:17 -0800

    Add User, Plant, Log to models.py

---
+++ flask/app/models.py
@@ -0,0 +1,107 @@
+#flask/app/models.py
+
+from flask_login import UserMixin
+from flask_marshmallow import fields
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from app import db, ma
+
+user_plant_assoc_table = db.Table(
+    "user_plant_assoc",
+    db.Column("user_id", db.Integer, db.ForeignKey("users.id", ondelete="CASCADE")),
+    db.Column("plant_id", db.Integer, db.ForeignKey("plants.id", ondelete="CASCADE")),
+)
+
+
+class Plant(db.Model):
+    """
+    Create a table of plants
+    """
+
+    __tablename__ = "plants"
+
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String)
+
+    def __repr__(self):
+        return "<Plant: {}>".format(self.name)
+
+# class User(UserMixin, db.Model):
+class User(db.Model):
+    """
+    Create a table of users
+    """
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(28))
+    last_name = db.Column(db.String(50))
+
+    email = db.Column(db.String(60), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128))
+
+    token = db.Column(db.String(32), index=True, unique=True)
+    token_expiration = db.Column(db.DateTime)
+
+    # db relationships
+    plants = db.relationship("Plant", secondary=user_plant_assoc_table)
+    
+    # @property
+    # def password(self):
+    #     """
+    #     Prevent password from being accessed
+    #     """
+    #     raise AttributeError("password is not a readable attribute")
+
+    # @password.setter
+    # def password(self, password):
+    #     """
+    #     Set password to a hashed password
+    #     """
+    #     self.password_hash = generate_password_hash(password)
+
+    # def verify_password(self, password):
+    #     """
+    #     Check if hashed password matches actual password
+    #     """
+    #     return check_password_hash(self.password_hash, password)
+
+    def get_token(self, expires_in=3600):
+        now = datetime.utcnow()
+        if self.token and self.token_expiration > now + timedelta(seconds=60):
+            return self.token
+        self.token = base64.b64encode(os.urandom(24)).decode("utf-8")
+        self.token_expiration = now + timedelta(seconds=expires_in)
+        db.session.add(self)
+        return self.token
+
+    def revoke_token(self):
+        self.token_expiration = datetime.utcnow() - timedelta(seconds=1)
+
+    @staticmethod
+    def check_token(token):
+        user = User.query.filter_by(token=token).first()
+        if user is None or user.token_expiration < datetime.utcnow():
+            return None
+        return user
+
+    def __repr__(self):
+        return "<User: {}".format(self.name)
+
+
+class Log(db.Model):
+    """
+    Create a table of logs
+    """
+
+    __tablename__ = "logs"
+    
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    water_goal = db.Column(db.Integer)
+    water_consumed = db.Column(db.Integer)
+    date = db.Column(db.DateTime)
+
+    #relationships
+    user = db.relationship("User", backref="logs")
