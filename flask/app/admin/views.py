076dbec3615447e8074cceb8e23ccbced925525b
karinarigby <karina.a.rigby@gmail.com>
2021-01-31 13:09:47 -0800

    Add 'list_plants' in flask/app/admin/views.py

--- flask/app/admin/views.py
+++ flask/app/admin/views.py
@@ -1,7 +1,8 @@
 
 #flask/app/admin/views.py
 from flask import render_template, url_for
-from ..models import User
+from ..models import User, Plant
+from . import admin
 
 @admin.route("/users", methods=["GET", "POST"])
 def list_users():
@@ -9,12 +10,13 @@ def list_users():
     List all users
     """
     users = User.query.all()
-
-    return render_template("admin/users.html", users=users, title-"Users")
-
-"""
-- view all users
-- view a user
+    return render_template("admin/users.html", users=users, title="Users")
 
 
-"""
+@admin.route("/plants", methods=["GET", "POST"])
+def list_plants():
+    """
+    List all types of plants
+    """
+    plants = Plant.query.all()
+    return render_template("admin/plants.html", plants=plants, title="Plants")
