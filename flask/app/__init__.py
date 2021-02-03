83912654fc3fffb4dcb31bd446803fbee946dbab
karinarigby <karina.a.rigby@gmail.com>
2021-02-03 15:43:48 -0800

    Add 404 and 500 error handler

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -34,4 +34,20 @@ def create_app(config_name):
     # from .auth import auth as auth_blueprint
     # app.register_blueprint(auth_blueprint)
 
+    # from .api import api as api_blueprint
+    # app.register_blueprint(api_blueprint, url_prefix="/api/v0")
+    
+    @app.errorhandler(404)
+    def not_found(error):
+        return render_template('errors/404.html', title='Page Isn\'t Found Dawg')
+
+    # Do some OAuth instead of Flask login manager
+
+    @app.errorhandler(500)
+    def internal_server_error(error):
+        db.session.rollback()
+        # if wants_json_response():
+            #return api_error_response(500)
+        return render_template('errors/500.html', title='Internal Server Error')
+
     return app
