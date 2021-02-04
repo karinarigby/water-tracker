e8b0097b69d325cbb7b579f5cd9a404438997600
karinarigby <karina.a.rigby@gmail.com>
2021-02-03 17:42:07 -0800

    Add plant view functions

--- flask/app/admin/views.py
+++ flask/app/admin/views.py
@@ -14,9 +14,37 @@ def list_users():
 
 
 @admin.route("/plants", methods=["GET", "POST"])
-def list_plants():
+def view_plants():
     """
     List all types of plants
     """
     plants = Plant.query.all()
-    return render_template("admin/plants.html", plants=plants, title="Plants")
+    return render_template("plant/plants.html", plants=plants, title="Plants")
+
+@admin.route("/plants/add", methods=["GET", "POST"])
+def add_plant():
+    """
+    Add a plant to the database
+    """
+    return render_template("plant/plant.html", title="Add a New Plant")
+
+@admin.route("/plants/edit/<int:id>", methods=["GET", "POST"])
+def edit_plant(id):
+    """
+    Edit a plant with the id
+    """
+    plant = Plant.query.get_or_404(id)
+    return render_template("plant/plant.html", title="Edit Plant {}".format(plant.type.data))
+
+@admin.route("/plants/delete/<int:id>", methods=["GET", "POST"])
+def delete_plant(id):
+    """
+    Delete a plant with the id
+    """
+    plant = Plant.query.get_or_404(id)
+    plant_type = plant.type
+    db.session.delete(plant)
+    db.session.commit()
+    flash('You have successfully deleted plant type {}'.format(plant_type))
+
+    return redirect(url_for("admin.view_plants"))
