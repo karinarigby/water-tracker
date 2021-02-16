612958b5b23ba19868121a423ae8bdf2fe13c973
karinarigby <karina.a.rigby@gmail.com>
2021-02-15 23:08:35 -0800

    Add initial progress bar shell functions - main.js

---
+++ flask/app/static/js/main.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+    var percentage = getUserPercentage();
+    var amount = 0;
+    
+    //retrieve the percentage of the current user's day water-percentage
+    function getUserPercentage(){
+        var progressBar = $("#progressBar");
+        $.ajax(
+            //send the get request to the db 
+        ).done(function(data){
+            updateProgressBarPercentage(data.percentage)
+        });
+    }
+
+    function adjustUserWaterAmount(amount){
+
+    }
+
+    //update the progress bar on the page
+    function updateProgressBarPercentage(percentage){
+
+    }
+
+    $("#water-container-s").click(function (){
+        console.log("Small container pressed");
+
+        adjustUserWaterAmount(225);
+        percentage = getUserPercentage();
+        updateProgressBarPercentage(percentage);
+    
+
+    });
+
+    $("#water-container-m").click(function (){
+       console.log("Med container pressed");
+       adjustUserWaterAmount(500);
+       percentage = getUserPercentage();
+       updateProgressBarPercentage(percentage);
+    });
+
+    $("#water-container-l").click(function (){
+        console.log("Large container pressed");
+        adjustUserWaterAmount(750);
+        percentage = getUserPercentage();
+        updateProgressBarPercentage(percentaage);
+    });
+});
